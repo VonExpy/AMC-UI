1a4d517a88d8d1015f4797d91d2eb77528919cb1
veera.arun-sameer-kumar@capgemini.com-at-028343876427 <veera.arun-sameer-kumar@capgemini.com>
2021-06-21 00:41:11 +0530

    Users - Personal UI, Forms Input UI

--- src/app/modules/users/details/details.component.ts
+++ src/app/modules/users/details/details.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
@@ -8,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 export class DetailsComponent implements OnInit {
 
   constructor() { }
-
+  edit: any = false;
+  editform() {
+    this.edit = !this.edit;
+  }
   ngOnInit(): void {
   }
   config = {
@@ -39,9 +41,43 @@ export class DetailsComponent implements OnInit {
       unchecked: "#ffffff"
     }
   };
+  toggle = {
+    value: true,
+    name: "test",
+    disabled: false,
+    height: 18,
+    width: 35,
+    margin: 1,
+    fontSize: 13,
+    speed: 300,
+    color: {
+      checked: "#1B6E3F",
+      unchecked: "#cccccc"
+    },
+    switchColor: {
+      checked: "#F8F8F8",
+      unchecked: "#F8F8F8"
+    },
+    labels: {
+      unchecked: "",
+      checked: ""
+    },
+    checkedLabel: "",
+    uncheckedLabel: "",
+    fontColor: {
+      checked: "#fafafa",
+      unchecked: "#ffffff"
+    }
+  };
   public scrollbarOptions = {
     theme: 'dark-3',
     // theme: 'minimal-dark',
+    autoHideScrollbar: true,
+    scrollButtons: { enable: false }
+  };
+  public scrollbarOptionsx = {
+    // theme: 'dark-3',
+    theme: 'minimal-dark',
     autoHideScrollbar: true,
     scrollButtons: { enable: false }
   };
