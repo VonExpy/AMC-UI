756c2e86b7bba0857ff045db34e7cf69059acfd0
santhosh kumar <santhosh.kolli@vonexpy.com>
2021-06-25 17:35:54 +0530

    side nav changes

--- src/app/modules/shared/components/profile/profile.component.ts
+++ src/app/modules/shared/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
+import { SharedService } from '../../services/shared.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  constructor(private sharedService:SharedService) { }
 
   ngOnInit(): void {
+  }
+
+  openSideNav(){
+    this.sharedService.sideNav("open");
   }
 
 }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { SharedComponent } from './components/shared/shared.component';
 import { OrdersComponent } from './components/orders/orders.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
     SharedComponent,
     OrdersComponent,
-    ProfileComponent
+    ProfileComponent,
+    SideNavComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [OrdersComponent,ProfileComponent]
+  exports: [OrdersComponent,ProfileComponent,SideNavComponent]
 })
 export class SharedModule { }
