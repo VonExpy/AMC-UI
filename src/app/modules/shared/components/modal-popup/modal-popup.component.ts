b6b88c9b152f245a5e378f70c941d14195b50f64
santhosh kumar <santhosh.kolli@vonexpy.com>
2021-07-01 15:49:11 +0530

    added modal pop up

---
+++ src/app/modules/shared/components/modal-popup/modal-popup.component.ts
@@ -0,0 +1,39 @@
+import { Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { SharedService } from '../../services/shared.service';
+
+@Component({
+  selector: 'app-modal-popup',
+  templateUrl: './modal-popup.component.html',
+  styleUrls: ['./modal-popup.component.scss']
+})
+export class ModalPopupComponent implements OnInit {
+  @ViewChild('viewContainerRef', { static: true, read: ViewContainerRef }) entry!: ViewContainerRef;
+  modalDetails: any = {
+    heading: '',
+    action: '',
+    size: ''
+  }
+  constructor(private sharedService: SharedService,
+    private resolver: ComponentFactoryResolver) { }
+
+  ngOnInit(): void {
+    this.sharedService.customObservable.subscribe((res) => {
+      console.log(res,'res')
+      this.modalDetails = res
+      if (res.action == 'open') {
+        this.openModal(res)
+      }
+    }
+    );
+  }
+
+  openModal(res: any) {
+    this.entry.clear();
+    const factory = this.resolver.resolveComponentFactory(res.component);
+    this.entry.createComponent(factory);
+  }
+
+  closeModal(){
+    this.modalDetails.action = 'close'
+  }
+}
