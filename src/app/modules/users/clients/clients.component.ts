5211515b1ca8adcb2c1e2cb09c2d13e620aa1a3d
veera.arun-sameer-kumar@capgemini.com-at-028343876427 <veera.arun-sameer-kumar@capgemini.com>
2021-07-02 19:48:34 +0530

    Clients UI

--- src/app/modules/users/clients/clients.component.ts
+++ src/app/modules/users/clients/clients.component.ts
@@ -9,7 +9,13 @@ export class ClientsComponent implements OnInit {
 
   constructor() { }
 
+  data = [0,1,2,3,4,5,6,7,8,9,10]
   ngOnInit(): void {
   }
 
+  public scrollbarOptions = {
+    theme: 'dark-3',
+    autoHideScrollbar: true,
+    scrollButtons: { enable: false }
+  };
 }
