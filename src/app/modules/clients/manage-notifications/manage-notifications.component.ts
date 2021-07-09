9ec302cb2f9d0b78bd4b2a0a70a3008b21116c0c
santhosh kumar <santhosh.kolli@vonexpy.com>
2021-07-09 16:01:15 +0530

    added manage noftifications page

---
+++ src/app/modules/clients/manage-notifications/manage-notifications.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../../shared/services/shared.service';
+import { StaticMasterService } from '../../shared/services/static-master.service';
+import { EditNotificationsComponent } from './edit-notifications/edit-notifications.component';
+
+@Component({
+  selector: 'app-manage-notifications',
+  templateUrl: './manage-notifications.component.html',
+  styleUrls: ['./manage-notifications.component.scss']
+})
+export class ManageNotificationsComponent implements OnInit {
+
+
+  constructor(private sharedService: SharedService, public staticService: StaticMasterService) { }
+
+  ngOnInit(): void {
+  }
+
+  openSideNav() {
+    this.sharedService.sideNav({
+      action: 'open', component: EditNotificationsComponent, width: "500px"
+    });
+  }
+
+}
