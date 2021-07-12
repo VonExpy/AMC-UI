656357dad5b9280e569f5718aebc66ce3ed1a2eb
santhosh kumar <santhosh.kolli@vonexpy.com>
2021-07-12 14:59:54 +0530

    appraisers edit, pref, exclusion UI

--- src/app/modules/clients/appraisers/edit-appraisers/edit-appraisers.component.ts
+++ src/app/modules/clients/appraisers/edit-appraisers/edit-appraisers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-appraisers',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-appraisers.component.scss']
 })
 export class EditAppraisersComponent implements OnInit {
-
-  constructor() { }
+  appraiserCompanies = [{ name: 'BEN ABRAMS', id:1 }, { name: '3-D Appraisal Inc.', id:2 }]
+  selected = [1,2]
+  appraisalForm!:FormGroup
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initAppraisalForm()
   }
+
+  initAppraisalForm() {
+    this.appraisalForm = this.fb.group({
+      appComps: [this.selected, Validators.required]
+    });
+  }
+
 
 }
