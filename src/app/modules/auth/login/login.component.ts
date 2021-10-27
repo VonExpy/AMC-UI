687605e634e23fa4bdc65840cd3968812ce9fc6e
unknown <varun@vonexpy.com>
2021-10-27 19:12:26 +0530

    Status & Order summary

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -7,24 +7,25 @@ import { ToastrService } from 'ngx-toastr';
 import { LoaderService } from '../../shared/services/loader.service';
 import { AuthService } from '../services/auth.service';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  loginForm!: FormGroup
-  stepper = false
-  submitted = false
+  loginForm!: FormGroup;
+  stepper = false;
+  submitted = false;
   returnUrl!: string;
 
-  constructor(private fb: FormBuilder,
+  constructor(
+    private fb: FormBuilder,
     private router: Router,
     private loaderService: LoaderService,
     private route: ActivatedRoute,
     private toastr: ToastrService,
-    private auth: AuthService) {
+    private auth: AuthService
+  ) {
     // redirect to dashboard if already logged in
     // console.log(this.auth.currentUserAttributes,'curent user value')
     // if (this.auth.currentUserAttributes) {
@@ -33,29 +34,31 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.initLoginForm()
+    this.initLoginForm();
     // get return url from route parameters or default to '/'
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
-
   initLoginForm() {
     this.loginForm = this.fb.group({
-      email: ['', Validators.compose([
-        Validators.required,
-        Validators.pattern("\\b[\\w.%-]+@[-.\\w]+\\.[A-Za-z]{2,4}\\b")
-      ])],
-      password: ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(6)
-      ])],
+      email: [
+        'santhoshca@yopmail.com',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('\\b[\\w.%-]+@[-.\\w]+\\.[A-Za-z]{2,4}\\b'),
+        ]),
+      ],
+      password: [
+        'India@123',
+        Validators.compose([Validators.required, Validators.minLength(6)]),
+      ],
     });
   }
 
   async onSubmit(form: FormGroup) {
     this.submitted = true;
     if (this.f.email.valid) {
-      this.stepper = true
+      this.stepper = true;
     }
     // stop here if form is invalid
     if (form.invalid) {
@@ -63,19 +66,26 @@ export class LoginComponent implements OnInit {
     }
 
     try {
-      this.loaderService.isLoading.next(true)
-      const currentUser = await this.auth.signIn(this.f.email.value, this.f.password.value)
-      this.toastr.success('Login Successful', `Hi ${currentUser.attributes.name}`)
+      this.loaderService.isLoading.next(true);
+      const currentUser = await this.auth.signIn(
+        this.f.email.value,
+        this.f.password.value
+      );
+      this.toastr.success(
+        'Login Successful',
+        `Hi ${currentUser.attributes.name}`
+      );
       // if(currentUser.attributes['custom:user_type'] == 'BORROWER'){
       //   return this.router.navigate(['/promissory']);
       // }
       this.router.navigate([this.returnUrl]);
-    } catch (e) {
-      this.toastr.error(e.message)
-      this.loaderService.isLoading.next(false)
+    } catch (e: any) {
+      this.toastr.error(e.message);
+      this.loaderService.isLoading.next(false);
     }
   }
 
-  get f() { return this.loginForm.controls; }
-
+  get f() {
+    return this.loginForm.controls;
+  }
 }
--- src/app/modules/orders/order-view/status-nav/status-nav.component.ts
+++ src/app/modules/orders/order-view/status-nav/status-nav.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+import { StaticMasterService } from '../../../shared/services/static-master.service';
 
 @Component({
   selector: 'app-status-nav',
   templateUrl: './status-nav.component.html',
-  styleUrls: ['./status-nav.component.scss']
+  styleUrls: ['./status-nav.component.scss'],
 })
 export class StatusNavComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  toggle: any = {};
+  @Output() onToggle = new EventEmitter<any>();
+  isActive: boolean = false;
+  constructor(private staticService: StaticMasterService) {
+    this.toggle = this.staticService.toggle('');
   }
 
+  ngOnInit(): void {}
 }
