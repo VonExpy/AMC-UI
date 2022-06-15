63d2b49cf8e5b37f9e44608bde682e1d8ea2ca31
dineshvonexpy <dinesh@vonexpy.com>
2022-06-15 12:57:21 -0500

    update DB string

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
 // const connectionURL = 'mongodb://127.0.0.1:27017/promissory'
-const connectionURL = process.env.DB
+const connectionURL = 'mongodb+srv://vonexpy:India@123@cluster0.xj3k6m5.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(connectionURL,{useNewUrlParser:true,useCreateIndex:true,useFindAndModify:false,useUnifiedTopology: true})
